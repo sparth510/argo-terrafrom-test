57f73db13c74d88bf2855c7de8f91acdcb3180ac
Parth Shah <30410503+sparth510@users.noreply.github.com>
2022-05-12 19:40:35 +0530

    Update pod.tf

--- pod.tf
+++ pod.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0"
-    }
-  }
-}
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
